a45b5ec66012ae057eb274b808bc69ec113c5681
Andrei Cureliuc <chezandrei@gmail.com>
2017-03-20 13:59:38 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/*(function() {
+            
+            var documentEl = $(document),
+                parallaxBg = $('header');
+            
+            documentEl.on('scroll', function() {
+                var currScrollPos = documentEl.scrollTop();
+                parallaxBg.css('background-position', '0 ' + -currScrollPos/4 + 'px');
+            });
+            
+            
+            
+        })();*/
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function openNav1() {
+    document.getElementById("myNav2").style.height = "100%";
+}
+
+function closeNav1() {
+    document.getElementById("myNav2").style.height = "0%";
+}
